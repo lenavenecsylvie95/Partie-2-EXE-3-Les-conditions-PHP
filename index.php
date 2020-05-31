60870db52ddf981e7cf4a5c86ba69dac15fbaf93
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-31 10:27:19 +0200

    ok3

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="utf-8" />
+  <title>Partie 2 EXE 3 php </title>
+</head>
+<body>
+  <?php
+  $gender = 'Homme';
+  if ($gender != 'Homme') {
+    ?>
+    <p>c'est une développeuse</p>
+    <?php
+  }
+  else  {
+    ?>
+    <p>c'est un développeur</p>
+    <?php
+  }
+  ?>
